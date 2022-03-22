8bc4b81106a55ffb8a0a6417de30aa6a6162d5d0
garulraj <sendtomr.arulraj@gmail.com>
2022-03-22 21:27:55 +0530

    another

---
+++ Employee.java
@@ -0,0 +1,14 @@
+-package tamilnadu.Kanchipuram;
+import tamilnadu.chennai.office;
+public class Employee
+{
+	public static void main(String[] args)
+	{
+		office of = new office();
+		//of.work();
+		//System.out.println(of.ta);
+		of.work_from_home();
+		//System.out.println(of.buscuits);
+	}
+	
+}
