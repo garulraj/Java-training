34f26e40f2065182a67a6e3c57ba6473963495df
garulraj <sendtomr.arulraj@gmail.com>
2022-03-25 15:19:59 +0530

    my learning

---
+++ BlockDemo.java
@@ -0,0 +1,14 @@
+public class BlockDemo
+{
+	{
+System.out.println("Non static Bloack");
+}
+static
+{
+System.out.println("static block");
+}
+public static void main(String [] args)
+{
+//BlockDemo bd = new BlockDemo();
+}
+}
